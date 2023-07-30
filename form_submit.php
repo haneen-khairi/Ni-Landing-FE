dd3db323bc4d3b224bd4395e4a432339db60d5d9
mohammad.shbeilat <mohammad.shbeilat@chainreaction.ae>
2023-07-30 14:06:40 +0300

    database connection for contact_us table

--- form_submit.php
+++ form_submit.php
@@ -11,21 +11,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $phone_number = $_POST['phone_number'];
     $alias = $_POST['alias'];
     $answer = $_POST['answer'];
+    $ip_address = get_client_ip();
     $status = 'empty';
 
     if (!empty($name) || !empty($email) || !empty($phone_number) || !empty($alias) || !empty($answer)) {
+
+        $conn = mysqli_connect("localhost:8889", "root", "root",
+            "north_immegration") or die("Connection Error: " . mysqli_error($conn));
+        $sql = "INSERT INTO tbl_contact_us(name, email, phone_number, alias, answer, ip_address, date) VALUES ('" . $name . "', '" . $email . "', '" . $phone_number . "', '" . $alias . "', '" . $answer . "', '" . $ip_address . "', '" . date('Y-m-d H:i:s') . "')";
+        $result = mysqli_query($conn, $sql);
+        if (!$result) {
+            die(var_dump($conn->connect_error));
+        }
+
         // Initialize PHPMailer
         $mail = new PHPMailer(true);
+
         try {
             // SMTP Configuration
 
             $mail->isSMTP();
-            $mail->Host = 'sandbox.smtp.mailtrap.io';
+            $mail->Host = 'smtp.office365.com';
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+//            $mail->SMTPDebug = SMTP::DEBUG_SERVER;
             $mail->SMTPAuth = true;
-            $mail->Port = 465;
-            $mail->Username = '369ee7a49d0884';
-            $mail->Password = '3c3df1d1920e82';
+            $mail->Port = 587;
+            $mail->Username = 'suha@northimmigration.com';
+            $mail->Password = 'Jordan@2022';
 
             // Set From, To, Subject, and Body
             $mail->setFrom($email, 'Mailtrap Website');
@@ -69,11 +81,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
             $status = 'success';
         } catch (Exception $e) {
-            die(var_dump($e->getMessage()));
             $status = 'error';
         }
     }
     echo $status;
     die;
+}
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
 }
 ?>
